68ad92c7f8cec911645d1125dc98a9285c1e879d
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-03-15 21:52:30 +0300

    Doms

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const mainRemove= document.getElementById("main");
+mainRemove.remove();
+const newHeader=document.createElement("h1");
+newHeader.id="victory"
+newHeader.textContent="Emmanuel is the champion"
+document.body.append(newHeader);;
